775f81523b5a1ddc3bb0c10667178f46d2456008
RIG360 <134787168+RIG360@users.noreply.github.com>
2026-04-29 12:19:32 +0200

    upload

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bild01",
+      "name": "Bild01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.552883157924235,
+        "pitch": 0.4531345461351304,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1698129694273263,
+          "pitch": 0.24285341031996843,
+          "rotation": 0,
+          "target": "1-bild02"
+        },
+        {
+          "yaw": -1.1801009846260122,
+          "pitch": 0.1323585321586762,
+          "rotation": 0,
+          "target": "2-bild03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bild02",
+      "name": "Bild02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.8230419737281647,
+        "pitch": 0.38030870500635316,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5012917200694211,
+          "pitch": 0.36744873953946566,
+          "rotation": 0,
+          "target": "2-bild03"
+        },
+        {
+          "yaw": -2.7090773631428267,
+          "pitch": 0.22894094355511108,
+          "rotation": 0,
+          "target": "0-bild01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bild03",
+      "name": "Bild03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.8758264220719099,
+        "pitch": 0.8539658999835851,
+        "fov": 1.5863744529599246
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.603841618708322,
+          "pitch": 0.3017443063139851,
+          "rotation": 0,
+          "target": "1-bild02"
+        },
+        {
+          "yaw": 1.5812107591507356,
+          "pitch": 0.1321708816453544,
+          "rotation": 0,
+          "target": "0-bild01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Linz269_Flachdachsanierung_260429",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
